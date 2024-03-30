682aa6eec42586c9bdd829fb4e8ba6efd4cec2cd
vanndeth <vanndeth@gmail.com>
2024-03-30 23:34:26 +0700

    change logo

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -1,9 +1,5 @@
 import { Metadata } from 'next'
 import React from "react";
-export const metadata: Metadata = {
-	title: "ISTAD Ecommerce About Us",
-	description: "We are providing the best service for you!",
-}
 
 import {
   BiGlobe,
